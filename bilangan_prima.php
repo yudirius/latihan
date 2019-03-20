7ee2c6c6197dee8352ca2f03753bfc763b61c3dd
yudirius <yudirius@gmail.com>
2019-03-20 23:04:42 +0800

    Update bilangan_prima.php
    menghapus comment

--- bilangan_prima.php
+++ bilangan_prima.php
@@ -1,14 +1,9 @@
 <?php
-/*
-$x = 8;  
-$y = 4;
-
-echo $x % $y;
-
-*/$batas= 10000;
+$batas= 10000;
 $prima=2;
 $hasil_prima=2;
 //pengulangan berdasarkan nilai input
+
 $arr=array("2","3");
 for($a=4;$a<$batas;$a++){
 	$isprima=false;
